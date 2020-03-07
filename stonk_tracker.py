93bc35ea15e095562096d418bec23af1632b7f0d
Dan Venis <dan.ven@gmail.com>
2020-03-07 12:02:37 -0800

    Add option support

--- stonk_tracker.py
+++ stonk_tracker.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import pandas as pd
 import yfinance as yf
 
@@ -8,46 +10,53 @@ class Symbol:
   def __init__(self, name):
     self.name = name
     self.price = -1
-    #self._open = -1
     self.previous_close = -1
   
-  #@property
-  #def name(self):
-  #  return self._name
-
   @property 
   def percent_change(self):
     return (self.price / self.previous_close - 1) * 100
-  
-  #@property
-  #def price(self):
-  #  return self._price
 
-  #@price.setter
-  #def price(self, val):
-  #  self._price = val
+class OptionType(Enum):
+  CALL = 1
+  PUT = 2
+
+def val(pd_object):
+  return pd_object.values[0]
   
-  #@property
-  #def open(self):
-  #  return self._open
-  
-  #@open.setter
-  #def open(self, val):
-  #  self._open = val
-  
-def get_symbol(ticker):
-  symbol = Symbol(ticker)
+def get_stock(ticker):
+  symbol = None
   try:
     yft_info = yf.Ticker(ticker).info
+    symbol = Symbol(yft_info['symbol'])
     symbol.price = (yft_info['bid'] + yft_info['ask'])/2
     symbol.previous_close = yft_info['regularMarketPreviousClose']
   except Exception:
     print('failed to get {}'.format(ticker))
   return symbol
 
+
+
+def get_option(ticker, date, strike, option_type):
+  symbol = None
+  try:
+    yft_option_chain = yf.Ticker(ticker).option_chain(date)
+    if option_type == OptionType.CALL:
+      yft_options = yft_option_chain.calls
+    elif option_type == OptionType.PUT:
+      yft_options = yft_option_chain.puts
+    yft_option = yft_options[yft_options['strike'] == strike]
+
+    symbol = Symbol(val(yft_option['contractSymbol']))
+    symbol.price = val((yft_option['bid'] + yft_option['ask'])/2)
+    symbol.previous_close = 5 # TODO change symbol or something
+  except Exception:
+    print('failed to get {}'.format(ticker))
+  return symbol
+
 def draw_symbol(stdscr, row, symbol):
+  stdscr.clrtoeol()
   stdscr.addstr(row, 0, symbol.name)
-  stdscr.addstr(row, 10, '{:.2f}'.format(symbol.price))
+  stdscr.addstr(row, 20, '{:.2f}'.format(symbol.price))
 
   if symbol.percent_change > 0:
     percent_color = curses.color_pair(1)
@@ -55,19 +64,40 @@ def draw_symbol(stdscr, row, symbol):
     percent_color = curses.color_pair(2)
   else:
     percent_color = curses.color_pair(0)
-  stdscr.addstr(row, 20, '{:.2f}%'.format(symbol.percent_change), percent_color)
+  stdscr.addstr(row, 30, '{:.2f}%'.format(symbol.percent_change), percent_color)
 
 def draw_symbols(stdscr, symbols):
   for i, symbol in enumerate(symbols):
     draw_symbol(stdscr, i, symbol)
+  stdscr.refresh()
+
+################################################################################
+# Command Handlers
+################################################################################
 
 def follow_handler(stdscr, command, args):
   stdscr.addstr(6, 0, 'following!')
-  symbols.append(get_symbol(args[0]))
-  draw_symbols(stdscr, symbols)
+  symbol = get_stock(args[0])
+  if symbol is not None:
+    symbols.append(symbol)
+    draw_symbols(stdscr, symbols)
 
 def unfollow_handler(stdscr, command, args):
   stdscr.addstr(6, 0, 'unfollowing...')
+
+def follow_call_handler(stdscr, command, args):
+  # followoption SPY 2020-03-08
+  symbol = get_option(args[0], args[1], float(args[2]), OptionType.CALL)
+  if symbol is not None:
+    symbols.append(symbol)
+    draw_symbols(stdscr, symbols)
+
+def follow_put_handler(stdscr, command, args):
+  # followoption SPY 2020-03-08
+  symbol = get_option(args[0], args[1], float(args[2]), OptionType.PUT)
+  if symbol is not None:
+    symbols.append(symbol)
+    draw_symbols(stdscr, symbols)
 
 def exit_handler(stdscr, command, args):
   exit()
@@ -75,11 +105,36 @@ def exit_handler(stdscr, command, args):
 def unknown_command_handler(stdscr, command, args):
   stdscr.addstr(6, 0, 'unknown command')
 
+def read_command(stdscr):
+  max_row, _ = stdscr.getmaxyx()
+  stdscr.clrtoeol()
+  # Draw at the bottom
+  stdscr.addstr(max_row-1, 0, ':')
+  curses.echo()
+  command_input = stdscr.getstr(max_row-1, 1, 128).decode('utf-8')
+  curses.noecho()
+
+  command_input = command_input.split(' ')
+  command = UNKNOWN_COMMAND
+  args = []
+
+  if len(command_input) > 0:
+    command = command_input[0].lower()
+  if command not in command_handlers:
+    command = UNKNOWN_COMMAND
+  if len(command_input) > 1:
+    args = command_input[1:]
+ 
+  return (command, args)
+
+
 UNKNOWN_COMMAND = ''
 
 command_handlers = {
   'follow': follow_handler,
   'unfollow': unfollow_handler,
+  'followcall': follow_call_handler,
+  'followput': follow_put_handler,
   'exit': exit_handler,
   '': unknown_command_handler,
 }
@@ -105,29 +160,12 @@ def main(stdscr):
   curses.init_pair(1, curses.COLOR_GREEN, -1)
   curses.init_pair(2, curses.COLOR_RED, -1)
 
-  for row, symbol in enumerate(symbols):
-    draw_symbol(stdscr, row, symbol)
+  draw_symbols(stdscr, symbols)
 
   while True:
     key = stdscr.getkey()
     if key == ':':
-      # commands
-      stdscr.addstr(5, 0, key)
-      curses.echo()
-      command_input = stdscr.getstr(5, 1, 128).decode('utf-8')
-      curses.noecho()
-
-      command_input = command_input.split(' ')
-      command = UNKNOWN_COMMAND
-      args = []
-
-      if len(command_input) > 0:
-        command = command_input[0]
-      if command not in command_handlers:
-        command = UNKNOWN_COMMAND
-      if len(command_input) > 1:
-        args = command_input[1:]
-
+      command, args = read_command(stdscr)
       command_handlers[command](stdscr, command, args)
 
   stdscr.refresh()
