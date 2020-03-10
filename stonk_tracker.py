44880619be69d851252a8da4b5e815f623fb7349
Dan Venis <dan.ven@gmail.com>
2020-03-09 19:37:49 -0700

    Add manual refresh for stocks and options

--- stonk_tracker.py
+++ stonk_tracker.py
@@ -11,47 +11,73 @@ class Symbol:
     self.name = name
     self.price = -1
     self.previous_close = -1
-  
+
   @property 
   def percent_change(self):
     return (self.price / self.previous_close - 1) * 100
 
+class Stock(Symbol):
+  def __init__(self, name):
+    super().__init__(name)
+
+class Option(Symbol):
+  def __init__(self, name, expiry_date, strike_price, option_type):
+    super().__init__(name)
+    self.expiry_date = expiry_date
+    self.strike_price = float(strike_price)
+    self.type = option_type
+
 class OptionType(Enum):
+  UNKNOWN = 0
   CALL = 1
   PUT = 2
 
 def val(pd_object):
   return pd_object.values[0]
-  
-def get_stock(ticker):
-  symbol = None
-  try:
-    yft_info = yf.Ticker(ticker).info
-    symbol = Symbol(yft_info['symbol'])
-    symbol.price = (yft_info['bid'] + yft_info['ask'])/2
-    symbol.previous_close = yft_info['regularMarketPreviousClose']
-  except Exception:
-    print('failed to get {}'.format(ticker))
-  return symbol
 
+def log(s):
+  pass #print(s)
 
+def warn(s):
+  print('warning: {}'.format(s))
 
-def get_option(ticker, date, strike, option_type):
-  symbol = None
-  try:
-    yft_option_chain = yf.Ticker(ticker).option_chain(date)
-    if option_type == OptionType.CALL:
-      yft_options = yft_option_chain.calls
-    elif option_type == OptionType.PUT:
-      yft_options = yft_option_chain.puts
-    yft_option = yft_options[yft_options['strike'] == strike]
+def error(s):
+  print('ERROR: {}'.format(s))
 
-    symbol = Symbol(val(yft_option['contractSymbol']))
-    symbol.price = val((yft_option['bid'] + yft_option['ask'])/2)
-    symbol.previous_close = 5 # TODO change symbol or something
-  except Exception:
-    print('failed to get {}'.format(ticker))
-  return symbol
+def refresh_stock(stock):
+  log('refreshing data for stock {}'.format(stock.name))
+
+  yft_info = yf.Ticker(stock.name).info
+  stock.price = (yft_info['bid'] + yft_info['ask'])/2
+  stock.previous_close = yft_info['regularMarketPreviousClose']
+
+def refresh_option(option):
+  log('refreshing data for option {}'.format(option.name))
+
+  yft_option_chain = yf.Ticker(option.name).option_chain(option.expiry_date)
+  if option.type == OptionType.CALL:
+    yft_options = yft_option_chain.calls
+  elif option.type == OptionType.PUT:
+    yft_options = yft_option_chain.puts
+  yft_option = yft_options[yft_options['strike'] == option.strike_price]
+
+  option.price = val((yft_option['bid'] + yft_option['ask'])/2)
+  option.previous_close = option.previous_close + 1
+
+def refresh_symbol(symbol):
+  if isinstance(symbol, Stock):
+    refresh_stock(symbol)
+  elif isinstance(symbol, Option):
+    refresh_option(symbol)
+  else:
+    error('Unknown symbol {}'.format(symbol))
+
+def refresh_symbols(symbols):
+  for symbol in symbols:
+    try:
+      refresh_symbol(symbol)
+    except Exception:
+      error('failed to refresh {}'.format(symbol.name))
 
 def draw_symbol(stdscr, row, symbol):
   stdscr.clrtoeol()
@@ -76,28 +102,45 @@ def draw_symbols(stdscr, symbols):
 ################################################################################
 
 def follow_handler(stdscr, command, args):
-  stdscr.addstr(6, 0, 'following!')
-  symbol = get_stock(args[0])
-  if symbol is not None:
-    symbols.append(symbol)
+  stock = Stock(args[0])
+  log('adding stock {}'.format(stock))
+  try:
+    refresh_stock(stock)
+    symbols.append(stock)
     draw_symbols(stdscr, symbols)
+    log('added {}'.format(stock))
+  except Exception:
+    error('failed to add stock {}'.format(stock))
 
 def unfollow_handler(stdscr, command, args):
   stdscr.addstr(6, 0, 'unfollowing...')
 
 def follow_call_handler(stdscr, command, args):
   # followoption SPY 2020-03-08
-  symbol = get_option(args[0], args[1], float(args[2]), OptionType.CALL)
-  if symbol is not None:
-    symbols.append(symbol)
+  option = Option(args[0], args[1], args[2], OptionType.CALL)
+  log('adding call {}'.format(option))
+  try:
+    refresh_option(option)
+    symbols.append(option)
     draw_symbols(stdscr, symbols)
+    log('added {}'.format(option))
+  except Exception:
+    error('failed to add option {}'.format(option))
 
 def follow_put_handler(stdscr, command, args):
   # followoption SPY 2020-03-08
-  symbol = get_option(args[0], args[1], float(args[2]), OptionType.PUT)
-  if symbol is not None:
-    symbols.append(symbol)
+  option = Option(args[0], args[1], args[2], OptionType.PUT)
+  log('adding put {}'.format(option))
+  try:
+    refresh_option(option)
+    symbols.append(option)
     draw_symbols(stdscr, symbols)
+    log('added {}'.format(option))
+  except Exception:
+    error('failed to add put {}'.format(option))
+
+def refresh_handler(stdscr, command, args):
+  refresh_symbols(symbols)
 
 def exit_handler(stdscr, command, args):
   exit()
@@ -135,6 +178,7 @@ command_handlers = {
   'unfollow': unfollow_handler,
   'followcall': follow_call_handler,
   'followput': follow_put_handler,
+  'refresh': refresh_handler,
   'exit': exit_handler,
   '': unknown_command_handler,
 }
